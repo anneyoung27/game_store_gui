377ce47ca8b190156f2e22f4906963fdb15494d7
Injas Mahendra Berutu <63891089+anneyoung27@users.noreply.github.com>
2022-10-08 20:11:01 +0700

    Update create+insert.sql

--- create+insert.sql
+++ create+insert.sql
@@ -1,31 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 08 Jun 2021 pada 10.41
--- Versi server: 10.4.13-MariaDB
--- Versi PHP: 7.4.7
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `dopegamestore`
---
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `carts`
---
+-- Database: `gamestore`
 
 CREATE TABLE `carts` (
   `UserID` char(5) NOT NULL,
@@ -33,11 +7,6 @@ CREATE TABLE `carts` (
   `Quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `detailtransactions`
---
 
 CREATE TABLE `detailtransactions` (
   `TransactionID` char(5) NOT NULL,
@@ -45,9 +14,6 @@ CREATE TABLE `detailtransactions` (
   `Quantity` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data untuk tabel `detailtransactions`
---
 
 INSERT INTO `detailtransactions` (`TransactionID`, `GameID`, `Quantity`) VALUES
 ('TR001', 'GA001', 71),
@@ -55,11 +21,6 @@ INSERT INTO `detailtransactions` (`TransactionID`, `GameID`, `Quantity`) VALUES
 ('TR002', 'GA001', 140),
 ('TR002', 'GA002', 44);
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `games`
---
 
 CREATE TABLE `games` (
   `GameID` char(5) NOT NULL,
@@ -69,19 +30,11 @@ CREATE TABLE `games` (
   `GameStock` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data untuk tabel `games`
---
 
 INSERT INTO `games` (`GameID`, `GameName`, `GameType`, `GamePrice`, `GameStock`) VALUES
 ('GA001', 'Minecraft', 'RPG', 650000, 82),
 ('GA002', 'Dota 2', 'FPS', 4500000, 178);
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `headertransactions`
---
 
 CREATE TABLE `headertransactions` (
   `TransactionID` char(5) NOT NULL,
@@ -89,19 +42,10 @@ CREATE TABLE `headertransactions` (
   `TransactionDate` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data untuk tabel `headertransactions`
---
-
 INSERT INTO `headertransactions` (`TransactionID`, `UserID`, `TransactionDate`) VALUES
 ('TR001', 'US002', '2021-06-08'),
 ('TR002', 'US002', '2021-06-08');
 
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `users`
---
 
 CREATE TABLE `users` (
   `UserID` char(5) DEFAULT NULL,
@@ -115,37 +59,18 @@ CREATE TABLE `users` (
   `UserRole` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data untuk tabel `users`
---
 
 INSERT INTO `users` (`UserID`, `UserName`, `UserEmail`, `UserPassword`, `UserDOB`, `UserGender`, `UserAddress`, `UserPhone`, `UserRole`) VALUES
 ('US001', 'Liu Kang', 'admin', 'admin', NULL, 'Male', 'asdasdasdasd Street', '0920398193812319', 'Admin'),
 ('US002', 'Kung Lao', 'customer', 'customer', NULL, 'Male', 'binus Street', '012345678911', 'Customer');
 
---
--- Indexes for dumped tables
---
 
---
--- Indeks untuk tabel `carts`
---
 ALTER TABLE `carts`
   ADD PRIMARY KEY (`UserID`,`BeverageID`);
 
---
--- Indeks untuk tabel `detailtransactions`
---
 ALTER TABLE `detailtransactions`
   ADD PRIMARY KEY (`TransactionID`,`GameID`);
 
---
--- Indeks untuk tabel `headertransactions`
---
 ALTER TABLE `headertransactions`
   ADD PRIMARY KEY (`TransactionID`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
