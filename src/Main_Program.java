b48b0215e377c5a7710db41ebeefe67b243ebced
Injas Mahendra Berutu <63891089+anneyoung27@users.noreply.github.com>
2022-10-08 21:45:07 +0700

    Add files via upload

---
+++ src/Main_Program.java
@@ -0,0 +1,16 @@
+import frame.LoginForm;
+
+public class Main_Program {
+    Main_Program(){
+
+        /* APPLICATION START - RUN!
+         */
+        LoginForm loginForm = new LoginForm();
+        loginForm.setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+      new Main_Program();
+    }
+}
